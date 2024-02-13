2a2c1a605dcc84cdb6411df338e14276e934afe5
Pratik Nawale <pratiknawale03@gmail.com>
2024-02-13 20:02:44 +0530

    minior changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
-import Navbar from "./components/LandingPage/Navbar/Navbar";
 
 export default function App() {
   return (
     <>
-      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
       </Routes>
--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar/Navbar";
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
-  )
-}
+    <>
+      <Navbar />
+    </>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
