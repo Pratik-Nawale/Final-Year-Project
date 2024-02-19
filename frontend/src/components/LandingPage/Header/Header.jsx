6fa7793cca521d302d6baeec96e8609e7624b588
unknown <virajchopade527@gmail.com>
2024-02-19 21:12:18 +0530

    Update Login and signup

--- frontend/src/components/LandingPage/Header/Header.jsx
+++ frontend/src/components/LandingPage/Header/Header.jsx
@@ -1,13 +1,18 @@
 import "./Header.css";
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const Header = () => {
+  const navigate = useNavigate();
+  const redirectPage = async () => {
+    navigate(`/SignUp-Login`);
+  };
+
   return (
     <div id="header-container">
       <div id="header-heading-container">
         <h1>Elevate Your Game with Sportz - Your All-in-One Sports Hub!</h1>
         {/* <button class="button-54" role="button">Button 54</button> */}
-        <button id="header-sign-up-button" role="button">
+        <button id="header-sign-up-button" role="button" onClick={redirectPage} >
           Sign Up
         </button>
       </div>
