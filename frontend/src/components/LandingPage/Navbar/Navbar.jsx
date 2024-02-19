124cb5f8139c5d8844fde04cee4353a9b7398b6d
Pratik Nawale <pratiknawale03@gmail.com>
2024-02-19 09:02:13 +0530

    Added Navbar and Styled it

--- frontend/src/components/LandingPage/Navbar/Navbar.jsx
+++ frontend/src/components/LandingPage/Navbar/Navbar.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
+import "./Navbar.css";
+
+const navElements = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "Features",
+    link: "/",
+  },
+  {
+    title: "Pricing",
+    link: "/",
+  },
+  {
+    title: "About",
+    link: "/",
+  },
+  {
+    title: "Contact",
+    link: "/",
+  },
+];
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [clicked, setclicked] = useState(false);
 
-export default Navbar
+  const handleClick = () => {
+    console.log("clicked");
+    if (clicked) {
+      setclicked(false);
+    } else if (!clicked) {
+      setclicked(true);
+    }
+  };
+
+
+  return (
+    <nav id="nav-container">
+      <Link href="">SPORTZ</Link>
+      <div>
+        <ul id="nav-elements-container" className={clicked? "nav-elements-container active": "nav-elements-container"}>  
+          {navElements.map((item, key) => (
+            <li>
+              <NavLink className="active" to={item.link} key={key}>
+                {item.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div id="sidebar" onClick={handleClick}>
+        <i id="bar" className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
