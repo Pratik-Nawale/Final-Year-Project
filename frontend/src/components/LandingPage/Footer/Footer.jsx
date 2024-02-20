c2af84a1b4f94ccd750469e89f85af3233ce3ee0
unknown <virajchopade527@gmail.com>
2024-02-20 08:30:19 +0530

    ADD FOOTER

--- frontend/src/components/LandingPage/Footer/Footer.jsx
+++ frontend/src/components/LandingPage/Footer/Footer.jsx
@@ -1,10 +1,65 @@
-import "./Footer.css"
-import React from 'react'
+import React from "react";
+import "./Footer.css";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
+  const elements = [
+    "Products",
+    "Services",
+    "Industries",
+    "Technologies",
+    "Partners",
+  ];
+  const otherLinks = [
+    {
+      title: "Sitemap",
+      link: "/sitemap",
+    },
+    {
+      title: "Term of Use",
+      link: "/term-of-use",
+    },
+    {
+      title: "Privacy policy",
+      link: "/privacy-policy",
+    },
+    {
+      title: "Security",
+      link: "/security",
+    },
+    {
+      title: "Accessibility",
+      link: "/accessibility",
+    },
+    {
+      title: "ContactUs",
+      link: "/contactus",
+    },
+  ];
   return (
-    <div>Footer</div>
-  )
-}
+    <div id="main-footer-div">
+      <div id="upper-footer">
+        
+        <div id="footer-nav">
+          {elements.map((ele, key) => (
+            <a href="#">{ele}</a>
+          ))}
+        </div>
+      </div>
+      <div id="lower-footer">
+        <div id="copy-right">&copy; SPORTZ. 2024. All Rights Reserved</div>
+        <div id="other-sections">
+          {otherLinks.map((ele, key) => (
+            <>
+              <NavLink to={ele.link} key={key}>
+                {ele.title}
+              </NavLink>
+            </>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
