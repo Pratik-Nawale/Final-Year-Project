ee267d51426ce913510380fb9e65ad7075fa228c
unknown <virajchopade527@gmail.com>
2024-02-20 08:38:25 +0530

    ADD CONTACTUS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import LoginAndOut from "./components/Login/LoginAndOut";
-// import  Contacts  from "./components/contact/Contacts";
+import  Contacts  from "./components/contact/Contacts";
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/SignUp-Login" element={<LoginAndOut />} />
-        {/* <Route path="/contactus" element={<Contacts/>} /> */}
+        <Route path="/contactus" element={<Contacts/>} />
       </Routes>
     </>
   );
