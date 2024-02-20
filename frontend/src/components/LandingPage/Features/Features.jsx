3f3c5074fb1c67bdc21b412170a7b0f8e06eb5eb
Pratik Nawale <pratiknawale03@gmail.com>
2024-02-20 14:40:29 +0530

    Add features and style it

--- frontend/src/components/LandingPage/Features/Features.jsx
+++ frontend/src/components/LandingPage/Features/Features.jsx
@@ -1,10 +1,66 @@
-import "./Feature.css"
-import React from 'react'
+import "./Feature.css";
+import React from "react";
+
+const FeatureElement1 = (item) => {
+  return (
+    <div id="features-main-container">
+      <div id="features-content-container">
+        <h1>{item.item.heading}</h1>
+        <p>
+        {item.item.info}
+        </p>
+      </div>
+      <div id="features-image-container">
+        <img src={item.item.image} alt="" />
+      </div>
+    </div>
+  );
+};
+const FeatureElement2 = (item) => {
+  return (
+    <div id="features-main-container">
+      <div id="features-image-container">
+        <img src={item.item.image} alt="" />
+      </div>
+      <div id="features-content-container">
+        <h1>{item.item.heading}</h1>
+        <p>
+          {item.item.info}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 const Features = () => {
-  return (
-    <div>Features</div>
-  )
-}
+  const Feature_contents = [
+    {
+      heading: "Win Prediction",
+      info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor natus earum, laboriosam nemo nisi hic magni reiciendis assumenda. Ipsa minus cumque pariatur vel omnis unde. Quam eaque quis cumque illo!",
+      image: "./images/Features/Prediction.png"
+      
+    },
+    {
+      heading: "Win Prediction",
+      info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor natus earum, laboriosam nemo nisi hic magni reiciendis assumenda. Ipsa minus cumque pariatur vel omnis unde. Quam eaque quis cumque illo!",
+      image: "./images/Features/Prediction.png"
+      
+    },
+    {
+      heading: "Win Prediction",
+      info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor natus earum, laboriosam nemo nisi hic magni reiciendis assumenda. Ipsa minus cumque pariatur vel omnis unde. Quam eaque quis cumque illo!",
+      image: "./images/Features/Prediction.png"
+      
+    },
+  ];
 
-export default Features
+  return (
+    <div>
+      <FeatureElement1 item={Feature_contents[0]}/>
+      <FeatureElement2 item={Feature_contents[1]}/>
+      <FeatureElement1 item={Feature_contents[2]}/>
+    </div>
+  );
+};
+
+export default Features;
